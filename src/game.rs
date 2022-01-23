db24169c586486b627ff7103d8cb75c3305ba9c6
tsukatana1 <72169823+tsukatana1@users.noreply.github.com>
2022-01-22 16:06:07 -0800

    Update game.rs
    Better rust.

--- src/game.rs
+++ src/game.rs
@@ -1,85 +1,83 @@
 use std::fmt::{Display, Formatter, Result as fmtResult};
 
-#[derive(Debug, Clone, PartialEq, Copy)]
+//The enumeration `Color`, which holds both colours of connect four, red and yellow.
+//Clone is derived to clone the current turn in the `Game` struct.
+#[derive(Debug, PartialEq, Clone)]
 pub enum Color {
-    Red,
-    Yellow
+    Red, Yellow
 }
 
-pub struct Player {
-    pub values: Vec<u16>,
-    pub color: Color
+//The struct `Player`. Sort of a wrapper for `Color`, containing the positions of the player, and their colour.
+#[derive(Debug, Clone)]
+pub struct Player(pub Vec<u16>, pub Color);
+
+/* The struct `Game`, which contains the current data of the game.
+players: a fixed size slice containing 2 `Player` structs.
+board: a vector of u16 (bitwise) of the connect 4 positions.
+turn: a `Color` which decides who the current player is.
+positions: a vector of bitwise of the columns.*/
+#[derive(Debug, Clone)]
+pub struct Game {
+    pub players: [Player; 2],
+    pub board: Vec<u16>,
+    pub turn: Color,
+    pub positions: Vec<u16>
 }
 
-
+//We want to implement `Display` for `Player`, to show the value of the current player.
 impl Display for Player {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmtResult {
-        write!(f, "{}", match self.color {
-            Color::Red => "Red",
-            Color::Yellow => "Yellow"
+        write!(f, "Player {}, choose a column to drop your value", match self.1 {
+            Color::Red => "\x1B[31mRed\x1B[0m",
+            Color::Yellow => "\x1B[33mYellow\x1B[0m"
         })
     }
 }
 
-
-pub struct Game {
-    pub players: Vec<Player>,
-    pub board: Vec<u16>,
-    pub turn: Color,
-}
-
+//Functions for `Game`.
 impl Game {
+    //new() makes a new instance of `Game`, with the default bitwise tables.
     pub fn new() -> Self {
-        let players = vec![
-            Player { values: vec![0, 0, 0, 0, 0, 0, 0, 0], color: Color::Red },
-            Player { values: vec![0, 0, 0, 0, 0, 0, 0, 0], color: Color::Yellow }
+        let players = [
+            Player(vec![0, 0, 0, 0, 0, 0, 0, 0], Color::Red),
+            Player(vec![0, 0, 0, 0, 0, 0, 0, 0], Color::Yellow)
         ];
 
         Self { 
             turn: Color::Red,
             players: players, 
             board: vec![0, 0, 0, 0, 0, 0, 0, 0],
+            positions: vec![1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4, 1 << 5, 1 << 6, 1 << 7]
         }
     }
-
-    pub fn advance(&mut self) {
+    
+    //switch() switches the current colour of `self.turn`.
+    pub fn switch(&mut self) {
         match self.turn {
             Color::Red => self.turn = Color::Yellow,
             Color::Yellow => self.turn = Color::Red
         }
     }
 
+    //create_board() creates the board to display.
     pub fn create_board(&self) -> String {
-        // starting from bottom to top, the positions represent a position in the column
-        let positions = vec![
-            1 << 0,
-            1 << 1,
-            1 << 2,
-            1 << 3,
-            1 << 4,
-            1 << 5,
-            1 << 6,
-            1 << 7,
-        ];
-        let mut string_board = String::from("  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |\n------------------------------------------------\n");
+        let mut string_board = String::new();
 
         for i in (0..8).rev() {
             for j in 0..8 {
-
-                if (positions[i] & self.players[0].values[j]) != 0 {
+                if (self.positions[i] & self.players[0].0[j]) != 0 {
                     string_board += "  \x1B[31mⓄ\x1B[0m  |";
                 }
-                else if (positions[i] & self.players[1].values[j]) != 0 {
+                else if (self.positions[i] & self.players[1].0[j]) != 0 {
                     string_board += "  \x1B[33mⓄ\x1B[0m  |";
                 }
                 else {
                     string_board += "     |";
                 }
             }
+
             string_board += "\n----- ----- ----- ----- ----- ----- ----- -----\n";
         }
-        
         string_board
     }
-
 }
